1329c3e0b6651ee69fa2a6378fa97b79d60d4b57
Pierre Marien <pierremarien29@gmail.com>
2023-09-08 11:51:42 +0200

    fin des premiers exercices

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1.0 et 1.1 mixer enssemble oupsi
+let age = 16;
+let day = 15;
+console.log(age + day);
+console.log(age / day);
+console.log(age - day);
+console.log(age % day);
+console.log(age * day);
+
+// 1.2
+
+let name = 'Lambert';
+let firstName = 'Gerard ';
+let city = 'Paris';
+
+console.log('Your name is ' + firstName + name + ' you are ' + age + ' since ' + day + ' days and you live in ' + city)
